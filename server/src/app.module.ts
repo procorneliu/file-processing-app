cb59dd1322e8de1aedcdead6eba4a1ec45ef3dc8
Corneliu <procorneliu@gmail.com>
2025-11-11 13:47:23 +0200

    configured @nestjs/config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FfmpegModule } from './ffmpeg/ffmpeg.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FfmpegModule],
+  imports: [
+    FfmpegModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
