77078b9181fa1131ba424597d2db835fa81d1e76
Corneliu <procorneliu@gmail.com>
2025-11-13 09:39:49 +0200

    backend config with @nestjs/throttler

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FfmpegModule } from './ffmpeg/ffmpeg.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: Number(config.getOrThrow<string>('THROTTLE_TTL')), // 60 sec
+          limit: Number(config.getOrThrow<string>('THROTTLE_LIMIT')), // 1 request
+        },
+      ],
+    }),
     FfmpegModule,
     ConfigModule.forRoot({
       isGlobal: true,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './exceptions/all-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.set('trust proxy', 'loopback');
   app.enableCors();
 
   app.setGlobalPrefix('/api');
